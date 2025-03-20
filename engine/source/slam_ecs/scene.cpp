8073de27ffd8ab8f0c46300015346418caf1cc8b
Charlie Haslam <chaslam@climaxstudios.com>
2025-03-20 12:33:28 +0000

    Proper entity_id and destroy

--- engine/source/slam_ecs/scene.cpp
+++ engine/source/slam_ecs/scene.cpp
@@ -5,40 +5,84 @@ namespace slam
 
 entity_id scene::new_entity()
 {
+    if (!m_free_entities.empty())
+    {
+        entity_index index = m_free_entities.back();
+        m_free_entities.pop_back();
+        entity_id id = create_entity_id(index, get_entity_version(m_entities[index].m_id));
+        m_entities[index].m_id = id;
+        return m_entities[index].m_id;
+    }
     m_entities.push_back({ m_entities.size(), component_mask() });
     return m_entities.back().m_id;
+}
+
+void scene::destroy_entity(entity_id id)
+{
+    if (m_entities[get_entity_index(id)].m_id != id)
+    {
+        // TODO this sould probably error as there is not information to check for a valid deletion being returned
+        return;
+    }
+
+    entity_id new_id = create_entity_id(entity_index(-1), get_entity_version(id) + 1);
+    m_entities[get_entity_index(id)].m_id = new_id;
+    m_entities[get_entity_index(id)].m_mask.reset();
+    m_free_entities.push_back(get_entity_index(id));
 }
 
 template<typename component_type>
 component_type* scene::assign_component(entity_id id)
 {
+    if (m_entities[get_entity_index(id)].m_id != id)
+    {
+        return nullptr;
+    }
+
     int component_id = get_component_id<component_id>();
 
     if (m_component_pools.size() <= component_id) // Expand the pool
     {
-        component_pool.resize(component_id + 1, nullptr);
+        m_component_pools.resize(component_id + 1, nullptr);
     }
     if (m_component_pools[component_id] == nullptr) // Create pool for new component
     {
         m_component_pools[component_id] = new component_pool(sizeof(component_type));
     }
 
-    compontnent_type* component = new (m_component_pools[component_id]->get(component_id)) component_type();
+    component_type* component = new (m_component_pools[component_id]->get(component_id)) component_type();
 
-    m_entities[id].m_mask.set(component_id);
+    m_entities[get_entity_index(id)].m_mask.set(component_id);
     return component;
 }
 
 template<typename component_type>
 component_type* scene::get_component(entity_id id)
 {
+    if (m_entities[get_entity_index(id)].m_id != id)
+    {
+        return nullptr;
+    }
+
     int component_id = get_component_id<component_type>();
-    if (!m_entities[id].mask.test(component_id))
+    if (!m_entities[get_entity_index(id)].m_mask.test(component_id))
     {
         // TODO error validation? Or maybe we are fine returning null and it is on the user to check....
         return nullptr;
     }
 
-    return static_cast<component_type*>(m_component_pools[component_id]->get(id));
+    return static_cast<component_type*>(m_component_pools[component_id]->get(get_entity_index(id)));
+}
+
+template<typename component_type>
+void scene::remove_component(entity_id id)
+{
+    if (m_entities[get_entity_index(id)].m_id != id)
+    {
+        return nullptr;
+    }
+
+    int component_id = get_component_id<component_type>();
+    m_entities[get_entity_index(id)].m_mask.reset(component_id);
 }
 };
--- engine/source/slam_ecs/scene.h
+++ engine/source/slam_ecs/scene.h
@@ -8,9 +8,30 @@
 namespace slam
 {
 
-typedef unsigned long long entity_id;
+typedef unsigned int entity_index;
+typedef unsigned int entity_version;
+typedef unsigned long long entity_id; // index first 32 bits, version last 32
 const int MAX_COMPONENTS = 32;
 typedef std::bitset<MAX_COMPONENTS> component_mask;
+
+inline entity_id create_entity_id(entity_index index, entity_version version)
+{
+    return ((entity_id)index << 32) | ((entity_id)version);
+}
+inline entity_index get_entity_index(entity_id id)
+{
+    return id >> 32;
+}
+inline entity_version get_entity_version(entity_id id)
+{
+    return (entity_version)id;
+}
+inline bool is_entity_valid(entity_id id)
+{
+    return (id >> 32) != entity_index(-1);
+}
+
+#define INVALID_ENTITY CreateEntityId(EntityIndex(-1), 0)
 
 struct entity_data
 {
@@ -22,6 +43,7 @@ class scene
 {
 public:
     entity_id new_entity();
+    void destroy_entity(entity_id id);
 
     template<typename component_type>
     component_type* assign_component(entity_id id);
@@ -29,8 +51,12 @@ public:
     template<typename component_type>
     component_type* get_component(entity_id id);
 
+    template<typename component_type>
+    void remove_component(entity_id id);
+
 private:
     std::vector<entity_data> m_entities;
+    std::vector<entity_index> m_free_entities;
     std::vector<component_pool> m_component_pools;
 };
 
